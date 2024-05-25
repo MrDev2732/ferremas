be45d999acddeb583d483df023cfc2dbad0c59c8
MrDev2732 <vin.orellana@duocuc.cl>
2024-05-25 05:05:30 -0400

    feature: Se crea la vista tienda

---
+++ apps/frontend-ferremas/src/app/tienda/tienda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tienda',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tienda.component.html',
+  styleUrl: './tienda.component.scss',
+})
+export class TiendaComponent {}
