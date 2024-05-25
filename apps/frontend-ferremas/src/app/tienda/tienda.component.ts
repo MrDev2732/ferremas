06f6b886278cdba223e9ef5fc5cae8f5424c8cb1
MrDev2732 <vin.orellana@duocuc.cl>
2024-05-25 19:25:44 -0400

    feature: Se intenta conectar al backend para obtener el endpoint de /obtener-productos

--- apps/frontend-ferremas/src/app/tienda/tienda.component.ts
+++ apps/frontend-ferremas/src/app/tienda/tienda.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../interfaces/product';
 
-@Component({
-  selector: 'app-tienda',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tienda.component.html',
-  styleUrl: './tienda.component.scss',
+import {ENV, Environment} from "../../enviroments/enviroment.dev";
+
+@Injectable({
+  providedIn: 'root'
 })
-export class TiendaComponent {}
+export class ConfigService {
+
+  private configUrl!: string;
+
+  constructor(private http: HttpClient,
+              @Inject(ENV) private environment: Environment,) {
+    this.configUrl = environment.apiUrl + 'obtener-productos'
+  }
+  getProducts( ): Observable<Product>{
+    return this.http.get<Product>(`${this.configUrl}`)
+  }
+
+}
