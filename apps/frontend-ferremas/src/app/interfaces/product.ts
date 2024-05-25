dd6b45aebd66e6fa2e3b98b36c06f0ab4ba1f0fa
MrDev2732 <vin.orellana@duocuc.cl>
2024-05-25 19:24:26 -0400

    feature: Se agrega el modelo de producto en el frontend

---
+++ apps/frontend-ferremas/src/app/interfaces/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    models: Product[];
+  }
+
+  export interface Product {
+    id:         string;
+    marca:      string;
+    nombre:     string;
+    categoria:  string;
+    stock:      number;
+    precio:     JSON;
+  }
+  
