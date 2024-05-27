e656e97ab54859cfa6cdc669aaa8accd559e25e1
MrMister2732 <vincenzorellanab@gmail.com>
2024-05-27 01:30:21 -0400

    refactor(frontend-ferremas): Se agrega el servicio ProductService para listar productos

--- apps/frontend-ferremas/src/app/components/tienda/tienda.component.ts
+++ apps/frontend-ferremas/src/app/components/tienda/tienda.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../services/product-list.service'; // Importa el servicio
+import { Producto } from '../../interfaces/producto'; // Importa la interfaz Producto
 
 @Component({
   selector: 'propilot-tienda',
@@ -8,4 +10,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './tienda.component.html',
   styleUrl: './tienda.component.scss',
 })
-export class TiendaComponent {}
+export class TiendaComponent implements OnInit {
+  productos: Producto[] = [];
+
+  constructor(private productService: ProductService) {} // Inyecta el servicio
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data: Producto[]) => {
+      this.productos = data;
+    });
+  }
+}
+
+
+
