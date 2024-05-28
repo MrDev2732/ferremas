c49dfaa15c807e8210a2a3eb658f52ded2291c1d
dakingh <infradabest@gmail.com>
2024-05-27 23:57:21 -0400

    Refactor: creacion del component de login

--- apps/frontend-ferremas/src/app/components/login/login.component.ts
+++ apps/frontend-ferremas/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginService } from '../../../services/login.service'; // Importa el servicio
+import { User } from '../../../app/interfaces/user';
 
 @Component({
   selector: 'propilot-login',
@@ -8,4 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private loginService: LoginService) {}
+
+  // Utilizar la función getUser del servicio LoginService
+  loginUser(username: string, password: string): void {
+    this.loginService.getUser(username, password).subscribe(
+      (response) => {
+        console.log('Respuesta del servidor:', response);
+      },
+      (error) => {
+        console.error('Error al obtener usuario:', error);
+      }
+    );
+  }
+}
