170cc6170b02e0f24f600de8a14d065a1ee7ccdb
dakingh <infradabest@gmail.com>
2024-05-27 23:55:56 -0400

    Refactor: se ajusto el endpoint del login

--- apps/backend-ferremas/backend/main.py
+++ apps/backend-ferremas/backend/main.py
@@ -89,7 +89,7 @@ async def login(username: str, password: str):
         user = session.query(User).filter(User.name == username).first()
         if user:
             if verify_password(password, user.password):
-                return {"message": "Inicio de sesión exitoso"}
+                return True
             else:
                 raise HTTPException(status_code=401, detail="Contraseña incorrecta")
         else:
