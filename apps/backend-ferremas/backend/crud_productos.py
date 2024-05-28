1d2b489e123dd6ab041df01f4199db33d1fb72bb
Draven <nicolasdraven91@gmail.com>
2024-05-27 21:57:11 -0400

    feature: Creacion crud producto

---
+++ apps/backend-ferremas/backend/crud_productos.py
@@ -0,0 +1,54 @@
+import random
+from datetime import datetime
+from sqlalchemy.orm import Session
+
+from backend.database.models import Product
+
+
+def create_product_db(session: Session, category, product, brand, image = None):
+    code_product = f'{product[:3].upper()}-{random.randint(100, 999)}'
+    now = datetime.now()
+    price = [
+            {
+                "date": now.isoformat(),
+                "price": random.randint(1, 100)
+            }
+        ]
+    db_product = Product(
+            id=code_product,
+            brand=brand,
+            name=product,
+            category=category,
+            stock=random.randint(1, 100),
+            image= image,
+            enabled=True,
+            created_date=now,
+            modified_date=now,
+            deleted_date=None,
+            price=price
+        )
+    session.add(db_product)
+    session.commit()
+    return db_product
+
+
+def update_product_db(session: Session, product_id: str, product: dict):
+    db_product = session.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        for key, value in product.items():
+            if hasattr(db_product, key) and getattr(db_product, key) != value:
+                if value is not None:
+                    setattr(db_product, key, value)
+        db_product.modified_date = datetime.now()
+        session.commit()
+        return db_product
+    return None
+
+
+def delete_product_db(session: Session, product_id: str):
+    db_product = session.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        session.delete(db_product)
+        session.commit()
+        return True
+    return False
