5592bdef81636a06dc98405ec149cc353d8edf19
dakingh <infradabest@gmail.com>
2024-05-27 22:45:06 -0400

    Featured: se agrega ruta login

--- apps/frontend-ferremas/src/app/app.routes.ts
+++ apps/frontend-ferremas/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'tienda', component: TiendaComponent },
+  { path: 'login', component: LoginComponent},
+  
 ];
