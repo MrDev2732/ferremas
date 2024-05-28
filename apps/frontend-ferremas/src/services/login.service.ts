f7653413d9d857261201427635f877c38ad6de99
dakingh <infradabest@gmail.com>
2024-05-27 22:46:22 -0400

    Feature: primera parte de implementacion del login/usuario

--- apps/frontend-ferremas/src/services/login.service.ts
+++ apps/frontend-ferremas/src/services/login.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../app/interfaces/user'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiUrl = 'http://localhost:8000/login';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUser(User.name, User.password):  
 }
+
+
