d418d1594de1067e42837a25262b1d4e5160025d
Draven <nicolasdraven91@gmail.com>
2024-05-28 00:55:31 -0400

    refactor: Se agrega enrutamiento a Admin

--- apps/frontend-ferremas/src/app/app.routes.ts
+++ apps/frontend-ferremas/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'tienda', component: TiendaComponent },
   { path: 'login', component: LoginComponent},
+  { path: 'admin', component: AdminComponent},
   
 ];
