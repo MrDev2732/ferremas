13daca7b5d2d61732f5971daa0df89a0f9f44a42
MrMister2732 <vincenzorellanab@gmail.com>
2024-05-29 01:44:13 -0400

    feature: Se agrega enrutamiento a pagina carrito

--- apps/frontend-ferremas/src/app/app.routes.ts
+++ apps/frontend-ferremas/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'tienda', component: TiendaComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'admin', component: AdminComponent},
-  
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'carrito', component: CarritoComponent },
 ];
 
 @NgModule({
