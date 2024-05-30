b2d59535df983fc85d6d28dfb2437e394c3842e0
AlexHernan7 <alexmazuela.v@gmail.com>
2024-05-30 13:54:08 -0400

    fix: se agrega funcionalidad para el cerrar sesion

--- apps/frontend-ferremas/src/app/app.component.ts
+++ apps/frontend-ferremas/src/app/app.component.ts
@@ -9,11 +9,18 @@ import { LoginService } from '../services/login.service';
 })
 export class AppComponent {
   title = 'frontend-ferremas';
+  userRole = '';
   
   constructor(private loginService: LoginService) { }
 
 
   logout(): void {
+    localStorage.setItem('isLoged', 'false')
     this.loginService.logout();
+  }
+
+  isUserLoggedIn(): boolean {
+    const isLoged = localStorage.getItem('isLoged');
+    return isLoged === 'true'; // Devuelve true si isLoged es 'true', de lo contrario, devuelve false
   }
 }
