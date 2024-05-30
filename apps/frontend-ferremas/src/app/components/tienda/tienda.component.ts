b2e3c3e8b5b5014b96c618f585f906bd1842d870
dakingh <infradabest@gmail.com>
2024-05-29 22:56:24 -0400

    Feature: se agregan components para carrito de compras

--- apps/frontend-ferremas/src/app/components/tienda/tienda.component.ts
+++ apps/frontend-ferremas/src/app/components/tienda/tienda.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product-list.service'; // Importa el servicio
 import { Product } from '../../interfaces/product'; // Importa la interfaz Product
+import { CarritoService } from '../../../services/carrito.service'
+
 
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.component.html',
   styleUrls: ['./tienda.component.scss'],
 })
+
 export class TiendaComponent implements OnInit {
   products: Product[] = [];
   valorDolar: number = 0;
 
-  constructor(private productService: ProductService) {} // Inyecta el servicio
-
+  constructor(private productService: ProductService, private carritoService: CarritoService) {} // Inyecta el servicio
+  
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data: Product[]) => {
       this.products = data;
@@ -21,6 +24,10 @@ export class TiendaComponent implements OnInit {
     this.productService.getDolar().subscribe((data: number) => {
       this.valorDolar = data;
     });
+  }
+
+  addToCart(product: Product) {
+    this.carritoService.addToCart(product);
   }
 }
 
