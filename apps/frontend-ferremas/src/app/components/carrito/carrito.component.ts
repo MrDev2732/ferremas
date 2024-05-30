62ff69e266d090f10f7bc2567d74f859f546317d
Draven <nicolasdraven91@gmail.com>
2024-05-30 02:20:48 -0400

    feature: Se modifica carrito de compra y se le da estilo

--- apps/frontend-ferremas/src/app/components/carrito/carrito.component.ts
+++ apps/frontend-ferremas/src/app/components/carrito/carrito.component.ts
@@ -3,6 +3,7 @@ import { CarritoService } from '../../../services/carrito.service';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../interfaces/product'
 import { ProductService } from 'apps/frontend-ferremas/src/services/product-list.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
@@ -16,7 +17,7 @@ export class CarritoComponent {
   valorDolar: number = 0;
   cart: { product: Product, quantity: number } [] = [];
 
-  constructor(private carritoService: CarritoService,private productService: ProductService) {
+  constructor(private carritoService: CarritoService, private productService: ProductService, private router: Router) {
     this.cart = this.carritoService.getCart();
   }
 
@@ -43,5 +44,9 @@ export class CarritoComponent {
     }, error => {
       console.error('Error creating the payment: ', error);
     });
+  }
+
+  goToShop() {
+    this.router.navigate(['/tienda']); // Ajusta la ruta según tu configuración
   }
 }
