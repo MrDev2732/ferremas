0a4aff6f089e3c482971b0a0116e47c67d965f73
dakingh <infradabest@gmail.com>
2024-05-30 00:22:09 -0400

    Feature: se agrega funcion de productos totales en service

--- apps/frontend-ferremas/src/services/carrito.service.ts
+++ apps/frontend-ferremas/src/services/carrito.service.ts
@@ -9,20 +9,24 @@ import { Product } from '../app/interfaces/product'
 
 export class CarritoService {
   private apiUrl = 'http://localhost:8000'; // Asegúrate de que la URL coincida con tu servidor backend
-  private cart: Product[] = [];
+  private cart: { product: Product, quantity: number }[] = [];
+
   constructor(private http: HttpClient) { }
 
-  
   createPayment(total: number): Observable<any> {
     return this.http.post(`${this.apiUrl}/create-payment?total=${total}`, {});  }
 
 
   addToCart(product: Product) {
-    this.cart.push(product);
+    const existingProduct = this.cart.find(item => item.product.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      this.cart.push({ product, quantity: 1 });
+    }
   }
-
-
-  getCart(): Product[] {
+  
+  getCart() {
     return this.cart;
   }
 }
