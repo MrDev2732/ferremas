a7e0bac559bb0b4e7cf8f76a2a8b4cff7e022f56
MrMister2732 <vincenzorellanab@gmail.com>
2024-05-30 02:54:39 -0400

    refactor: Se reconocen los usuarios para poder iniciar sesión

--- apps/frontend-ferremas/src/app/components/login/login.component.ts
+++ apps/frontend-ferremas/src/app/components/login/login.component.ts
@@ -1,64 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginService } from '../../../services/login.service'; // Importa el servicio
-import { User } from '../../../app/interfaces/user';
+import { LoginService } from '../../../services/login.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'propilot-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private loginService: LoginService,
-              private router: Router
-  ) {}
-
-/*   // Utilizar la función getUser del servicio LoginService
-  loginUser(username: string, password: string): void {
-    this.loginService.getUser(username, password).subscribe(
-      (response) => {
-        console.log('Respuesta del servidor:', response);
-      },
-      (error) => {
-        console.error('Error al obtener usuario:', error);
-      }
-    );
-  } */
+  constructor(private loginService: LoginService, private router: Router) {}
 
   loginUser(username: string, password: string): void {
     this.loginService.getUser(username, password).subscribe(
       (response: any) => {
-        // Extraer la información del usuario y su rol
-        const { name, rol } = response;
-        console.log('Usuario:', username);
-        console.log('Rol:', rol);
-  
-        // Redirigir según el rol del usuario
-        switch (rol) {
-          case 'admin':
-            this.router.navigate(['/admin']);
-            break;
-          case 'VENDEDOR':
-            this.router.navigate(['/vendedor']);
-            break;
-          case 'BODEGUERO':
-            this.router.navigate(['/bodega']);
-            break;
-          // Agrega casos para otros roles si es necesario
-          default:
-            // Si el rol no coincide con ninguno de los casos anteriores, manejar el escenario apropiadamente
-            console.error('Rol no reconocido:', rol);
-            // Por ejemplo, redirigir a una página de error o mostrar un mensaje al usuario
+        const userRole = response.rol; // Changed from 'userRole' to 'rol'
+        if (userRole) {
+          localStorage.setItem('userRole', userRole); // Store user role
+          this.redirectUser(userRole); // Redirect user based on role
+        } else {
+          console.error('User role not provided');
+          this.router.navigate(['/login']); // Redirect to login if no role provided
         }
       },
       (error) => {
-        console.error('Error al obtener usuario:', error);
+        console.error('Error getting user:', error);
+        this.router.navigate(['/login']); // Redirect to login on error
       }
     );
   }
-  
+
+  private redirectUser(role: string): void {
+    if (role) {
+      switch (role.toUpperCase()) {
+        case 'ADMINISTRADOR':
+          this.router.navigate(['/admin']);
+          break;
+        case 'VENDEDOR':
+          this.router.navigate(['/vendedor']);
+          break;
+        case 'BODEGUERO':
+          this.router.navigate(['/bodega']);
+          break;
+        default:
+          console.error('Unrecognized role:', role);
+          this.router.navigate(['/login']); // Redirect to login if role not recognized
+          break;
+      }
+    } else {
+      console.error('Role undefined');
+      this.router.navigate(['/login']); // Redirect to login if role is undefined
+    }
+  }
 }
