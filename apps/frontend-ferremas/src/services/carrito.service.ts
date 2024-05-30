48f47165c7aff8871dd501ebc1fed85b05df7aff
dakingh <infradabest@gmail.com>
2024-05-29 22:56:57 -0400

    Feature: se agrega array de product para ser usado en carrito de compras

--- apps/frontend-ferremas/src/services/carrito.service.ts
+++ apps/frontend-ferremas/src/services/carrito.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../app/interfaces/product'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CarritoService {
   private apiUrl = 'http://localhost:8000'; // Asegúrate de que la URL coincida con tu servidor backend
-
+  private cart: Product[] = [];
   constructor(private http: HttpClient) { }
 
+  
   createPayment(total: number): Observable<any> {
     return this.http.post(`${this.apiUrl}/create-payment?total=${total}`, {});  }
+
+
+  addToCart(product: Product) {
+    this.cart.push(product);
+  }
+
+
+  getCart(): Product[] {
+    return this.cart;
+  }
 }
